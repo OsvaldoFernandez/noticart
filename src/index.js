e5ea43a9875c988c540305d8dba24592574d0ea8
Osvaldo Fernandez <osvaldo.g.fernandez@gmail.com>
2025-10-19 12:35:20 -0300

    products implemented

--- src/index.js
+++ src/index.js
@@ -2,44 +2,56 @@ console.log("Hello World from uva!");
 
 
 import axios from "axios";
-
+axios.defaults.timeout = 50000; // Sets a global timeout of 5 seconds (5000 milliseconds)
 const TEST = false;
 
+let products = [
+	{ id: 'EAN123', name: 'Yogur' }, 
+	{ id: 'EAN456', name: 'Queso' },
+	{ id: 'EAN789', name: 'Ketchup' }
+]
+
+let results = [];
 
 
-const logSuccess = (position) => {
-  getSucursales(position.coords)
+const locSuccess = (position) => {
+  getSucursales(position.coords);
 }
 
-const logError = () => {
-  alert("Sorry, no position available.");
-}
+const locError = () => { alert("Sorry, no position available.") };
 
 
 const getLocation = () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(logSuccess, logError);
-  } else {
-    alert("Geolocation is not supported by this browser.");
-  }
+  (navigator.geolocation) ? navigator.geolocation.getCurrentPosition(locSuccess, locError) : locError();
 }
 
+const sucSuccess = (resp) => {
+	debugger;
+  getProductos(resp, products);
+}
 
 const getSucursales = (location) => {
+  const params = {lat: location.latitude, lng: location.longitude, offset: 0, limit: 30 }
 
-	return axios
-		.get('https://d3e6htiiul5ek9.cloudfront.net/prod/sucursales', {
-			params: {
-				lat: location.latitude,
-				lng: location.longitude,
-				offset: 0,
-				limit: 30,
-			},
-		})
-		.then((response) => response.data)
-		.catch((e) => {
-			console.log(e);
-		});
+	//axios.get('https://d3e6htiiul5ek9.cloudfront.net/prod/sucursales', { params }).then(sucSuccess)
+	new Promise((resolve, reject) => {resolve([1,2,3]); }).then(sucSuccess);
+}
+
+const prodSuccess = (resp, idsSucursales, productos) => {
+  results.push(resp);
+  getProductos(idsSucursales, productos);
+};
+
+const showResults = () => {
+	console.log(results);
+};
+
+const getProductos = (idsSucursales, productos) => {
+	if (productos.length === 0) return showResults();
+
+	const params = { id_producto: productos[0].id, array_sucursales: idsSucursales.join(','), limit: 30 };
+	//axios.get('https://d3e6htiiul5ek9.cloudfront.net/prod/producto', { params }).then((resp) => prodSuccess(resp, idsSucursales, productos));
+	new Promise((resolve, reject) => {resolve(productos.shift()) }).then((resp) => prodSuccess(resp, idsSucursales, productos));
 }
 
 getLocation();
